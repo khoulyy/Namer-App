2e73f7082677faf84595c6cc40213dc710f4c4ed
khoulyy <abdoelkhouly555@gmail.com>
2023-04-04 17:44:27 +0200

    Added const keyword to the app to increase performance

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
